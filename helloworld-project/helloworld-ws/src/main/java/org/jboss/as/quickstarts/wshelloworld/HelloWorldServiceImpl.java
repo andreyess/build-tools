ebe37c856dbe492fadc4ef293b892bc67bc2c6d4
Andrei Karpyza <akarpyza@localhost.localdomain>
2021-03-18 20:57:26 +0300

    Revert "Revert "Revert "Fix 2"""
    This reverts commit f0059f637123e4f70c784a3dcd602c2e94b89ce1.

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -21,6 +21,8 @@ import java.util.List;
 
 import javax.jws.WebService;
 
+import jdk.jfr.internal.tool.Print;
+
 /**
  * The implementation of the HelloWorld JAX-WS Web Service.
  *
@@ -37,17 +39,23 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public String sayZhive() {
-        return "Zhive!";
+        while (true){
+            System.out.println("Zhive!");
+            sayTheBestCountryInTheWorld();
+        }
     }
 
     @Override
     public String sayTheBestCountryInTheWorld() {
-        return "Belarus!";
+        while (true){
+            System.out.println("Belarus!");
+            sayZhive();
+        }
     }
 
     @Override
     public String sayHello() {
-        return "Hello!";
+        return "Hello world!";
     }
 
 
@@ -65,11 +73,6 @@ public class HelloWorldServiceImpl implements HelloWorldService {
     public String sayHelloToNames(final List<String> names) {
         sayZhive();
         sayTheBestCountryInTheWorld();
-
-        while (true) {
-            sayZhive();
-            sayTheBestCountryInTheWorld();
-        }
 
         return "Hello " + createNameListString(names);
     }
